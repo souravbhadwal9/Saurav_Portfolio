ee3dfc2d566c42706dc954f8283125ca7ac14232
Your Name <you@example.com>
2026-06-08 18:37:11 +0530

    mcommit

---
+++ app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <link rel="manifest" href="/manifest.json" />
+      <link rel="icon" href="/favicon.svg" />
+      <link rel="apple-touch-icon" href="/icons8-portfolio-48.png" />
+      <meta name="theme-color" content="#10b981" />
+    </>
+  );
+}
